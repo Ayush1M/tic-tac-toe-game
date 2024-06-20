7afa3ecbc507b1b973e2728306406b6e7b1a2b22
Ayush <96943871+Ayush1M@users.noreply.github.com>
2024-06-21 03:53:59 +0530

    added animation for log table

--- src/components/Log.tsx
+++ src/components/Log.tsx
@@ -9,7 +9,7 @@ const Log : FC<LogProps> = ({gameTurns}) => {
     return (
         <ul className="mt-12">
             {gameTurns.map((turn) =>
-                <li key={`${turn.square.row}${turn.square.col}`} className="text-white text-2xl max-lg:text-center"> 
+                <li key={`${turn.square.row}${turn.square.col}`} className="text-white text-2xl max-lg:text-center animate-slide-in-from-left"> 
                 {turn.player} selected {turn.square.col},{turn.square.row} 
                 </li>)}
         </ul>
--- tailwind.config.js
+++ tailwind.config.js
@@ -25,10 +25,15 @@ export default {
           "0%" : {transform : "scale(0)"},
           "80%" : {transform : "scale(1.1)"},
           "100%" : {transform : "scale(1)"}
+        },
+        slide : {
+          "0%" : {transform : "translateX(-80%)"},
+          "100%" : {transform : "translateX(0)"}
         }
       },
       animation : {
-        "pop-in" : "pop 0.5s cubic-bezier(0.68, -0.55, 0.65, 0.52) forwards"
+        "pop-in" : "pop 0.3s cubic-bezier(0.68, -0.55, 0.65, 0.52) forwards",
+        "slide-in-from-left" : "slide 0.5s cubic-bezier(0.075, 0.82, 0.165, 1) backwards"
       }
     },
   },
